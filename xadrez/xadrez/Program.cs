18910fed07cafbbbfc571014c1735211b1e98347
Alexandre Neves <alexandreneves98@outlook.com>
2021-02-15 19:53:42 -0300

    Começando a colocar peças no tabuleiro

--- xadrez/xadrez/Program.cs
+++ xadrez/xadrez/Program.cs
@@ -9,6 +9,10 @@ namespace xadrez
         {
             Tabuleiro t = new Tabuleiro(8, 8);
 
+            t.ColocarPeca(new Torre(Cor.Preto, t), new Posicao(0, 0));
+            t.ColocarPeca(new Torre(Cor.Preto, t), new Posicao(1, 3));
+            t.ColocarPeca(new Rei(Cor.Preto, t), new Posicao(2, 4));
+
             Tela.ImprimirTabuleiro(t);
 
             Console.WriteLine();
